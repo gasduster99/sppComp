c3c990f37e0076d38abe766b07312fc5fcef7385
gasduster99 <grunloh@soe.ucsc.edu>
2018-05-15 16:26:19 -0400

    a parallel version of the test script

--- try1/quickScripts/serverTest.r
+++ try1/quickScripts/serverTest.r
@@ -1,4 +1,12 @@
+suppressMessages(library(doParallel, quietly=FALSE))
+suppressMessages(library(foreach, quietly=FALSE))
+#
+threads = 48
 saveFile = '/tmp/test.csv' #RtmpZLgZmw/test.csv'
 out =  matrix(rep(1, 10000), nrow=100, ncol=100)
+#
 write.table(out, saveFile, sep=',', quote=F, row.names=F, col.names=F)
-while(T){ write.table(out, saveFile, sep=',', append=T, quote=F, row.names=F, col.names=F) }
+registerDoParallel(cores=threads)
+foreach( p=1:threads )%dopar%{
+	while(T){ write.table(out, saveFile, sep=',', append=T, quote=F, row.names=F, col.names=F) }
+}
