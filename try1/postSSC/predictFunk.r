93e139029cc3a6714f0d7292df0ca5ac4cd3b2d3
gasduster99 <grunloh@soe.ucsc.edu>
2018-06-12 20:57:12 -0700

    add image magic and switch to convert instead of pdftk

--- try1/postSSC/predictFunk.r
+++ try1/postSSC/predictFunk.r
@@ -361,7 +361,8 @@ plotPerf = function(preds, level,
 	#
 	if( save ){
 		# 
-		system(sprintf('pdftk %s/%s-%1.2f-Diagnostic-*%s.pdf output %s/%s-%1.2f-Diagnostic%s.pdf', path, path, round(level,2), saveString, path, path, round(level,2), saveString)) 
+		system(sprintf('convert %s/%s-%1.2f-Diagnostic-*%s.pdf %s/%s-%1.2f-Diagnostic%s.pdf', path, path, round(level,2), saveString, path, path, round(level,2), saveString)) 
+		#system(sprintf('pdftk %s/%s-%1.2f-Diagnostic-*%s.pdf output %s/%s-%1.2f-Diagnostic%s.pdf', path, path, round(level,2), saveString, path, path, round(level,2), saveString)) 
 		#system(sprintf('convert %s/%s-Diagnostic.pdf %s/%s-Diagnostic.png', path, path, path, path))
 	}
 }
