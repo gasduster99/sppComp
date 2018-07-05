785972ddbc17dba318e9e761e40b865383ea0410
gasduster99 <grunloh@soe.ucsc.edu>
2018-07-05 13:05:36 -0700

    added 25019781982M2

---
+++ sscRuns/copyRunPics.sh
@@ -0,0 +1,23 @@
+#mkdir 25019781982M1
+#cp /media/nick/extraBig/25019781982M1/*.csv 25019781982M1/
+#cp /media/nick/extraBig/25019781982M1/*.pdf 25019781982M1/
+#cp -r /media/nick/extraBig/25019781982M1/marginal* 25019781982M1/
+#cp -r /media/nick/extraBig/25019781982M1/year-species/ 25019781982M1/
+#cp -r /media/nick/extraBig/25019781982M1/gear-year-species/ 25019781982M1/
+#cp -r /media/nick/extraBig/25019781982M1/port-gear-qtr-year-species/ 25019781982M1/
+#git add 25019781982M1/
+#git commit -am "added 25019781982M1"
+copyRun(){
+	mkdir $1
+	cp /media/nick/extraBig/$1/*.csv $1/
+	cp /media/nick/extraBig/$1/*.pdf $1/
+	cp -r /media/nick/extraBig/$1/marginal* $1/
+	cp -r /media/nick/extraBig/$1/year-species/ $1/
+	cp -r /media/nick/extraBig/$1/gear-year-species/ $1/
+	cp -r /media/nick/extraBig/$1/port-gear-qtr-year-species/ $1/
+	git add $1/
+	git commit -am "added $1"
+}
+
+#
+copyRun 25019781982M2
